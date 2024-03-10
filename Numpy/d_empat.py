e3b93d3e54a7650853a899011c1e6d1891de2ec6
NathanAbrahamSinaga <nathansinaga2002@gmail.com>
2024-03-10 20:58:46 +0700

    d

--- Numpy/d_empat.py
+++ Numpy/d_empat.py
@@ -18,3 +18,18 @@ c = np.array([[1,2,3,4,5,6,7], [8,9,10,11,12,13,14]], dtype = np.float64)
 c.itemsize
 
 c.shape
+
+np.zeros((2,3))
+
+np.ones((2,3,4), dtype = np.int16)
+
+l=range(10)
+l[0]
+
+c.shape
+c.reshape
+
+c.ndim
+
+np.arange(1,4)
+np.arange(10, 30, 5)
